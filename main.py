6680efbc8b11b52d70090bd7e1a15ac67f32a814
RyAnPr1mE <ryan.j.manzo@gmail.com>
2026-04-24 09:57:04 -0400

    create welcome screen

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import helpers as helpers
 
 
 def main():
+    print("=" * 40)
+    print(" Welcome to the Media Review System ")
+    print("Rate, review, and explore your favorite media!")
+    print("=" * 40)
+    while (True):
+        
+        
     
 
     
