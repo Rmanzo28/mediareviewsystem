aa7c589bc2986db9c836612e453da2e432318084
RyAnPr1mE <ryan.j.manzo@gmail.com>
2026-04-24 10:12:16 -0400

    began match case statement in main

--- main.py
+++ main.py
@@ -1,13 +1,56 @@
 import helpers as helpers
+import logging as log
 
 
 def main():
-    print("=" * 40)
-    print(" Welcome to the Media Review System ")
-    print("Rate, review, and explore your favorite media!")
-    print("=" * 40)
-    while (True):
-        
+       
+        print("=" * 40)
+        print(" Welcome to the Media Review System ")
+        print("Rate, review, and explore your favorite media!")
+        print("=" * 40)
+        userchoice = input("What Would You Like To Do?(Enter/View/Sort)").lower()
+       
+
+        match userchoice:
+                case "enter":
+                         while (True):
+                                try:
+
+                                    data_dict:dict = helpers.collect_entries()
+                                    success:bool = helpers.write_to_file(data_dict)
+                                    if success:
+                                           break
+                                    elif not success:
+                                           print("File Write Failed")
+                case "view":
+                        
+                case "sort":
+                        
+                
+
+             
+
+       # while (True):
+           # try:
+
+             #   data_dict:dict = helpers.collect_entries()
+              #  success:bool = helpers.write_to_file(data_dict)
+              # if success:
+                     
+                     
+                     
+                     
+
+                
+
+
+
+
+
+
+
+
+
         
     
 
